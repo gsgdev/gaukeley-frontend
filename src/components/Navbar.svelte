1ccde2b93bef942cb8d2f06cd463aa137c2a3bc9
Johannes Bähr <b_and_j_gamers@gmx.de>
2023-02-15 22:02:27 +0100

    deleted some boilerplate files added a simple login page for testing

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,14 @@
+<nav>
+    <p>1</p>
+    <p>2</p>
+    <p>3</p>
+</nav>
+
+<style>
+    nav {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-end;
+        
+    }
+</style>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    let username = '';
+    let password = '';
+  
+    function handleSubmit() {   
+       console.log(`Username: ${username}, Password: ${password}`);
+    }
+
+</script>
+  
+<form on:submit|preventDefault={handleSubmit}>
+<label for="username">Username:</label>
+<input type="text" id="username" bind:value={username} />
+
+<label for="password">Password:</label>
+<input type="password" id="password" bind:value={password} />
+
+<button type="submit">Submit</button>
+</form>
+  

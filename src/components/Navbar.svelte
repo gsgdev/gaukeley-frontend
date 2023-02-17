15c69ac38bc01c150f58b88c6d44f54c57a3e6b7
Johannes Bähr <b_and_j_gamers@gmx.de>
2023-02-17 19:02:35 +0100

    changed navbar styles

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,7 +1,7 @@
 <nav>
 	<div class="right-nav">
-        logo
-    </div>
+		<p>logo</p>
+	</div>
 	<div class="left-nav">
 		<p>Features</p>
 		<p>Fuer Unternehme</p>
@@ -11,28 +11,35 @@
 
 <style>
 	nav {
+		font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
+			Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 		display: flex;
 		flex-direction: row;
-		justify-content: space-between; 
+		justify-content: space-between;
+		border: 1px solid red;
 	}
-    .left-nav > p {
-        margin-right: 1.25rem;
-        text-decoration: underline;
-    }
-    .left-nav > p:hover {
-        text-decoration: none;
-        cursor: pointer;
-    }
-    .login-button {
-        border: none;
-        background-color: white;
-        cursor: pointer;
-        font-size: 1rem;
-        margin-right: 0.5rem;
-    }
-    .left-nav {
-        display: flex;
-        flex-direction: row;
-    }
+	.right-nav > p {
+		vertical-align: middle;
+	}
 
+	.left-nav > p {
+		margin-right: 1.25rem;
+		text-decoration: underline;
+	}
+	.left-nav > p:hover {
+		text-decoration: none;
+		cursor: pointer;
+	}
+	.login-button {
+		border: none;
+		background-color: white;
+		cursor: pointer;
+		font-size: 1rem;
+		margin-right: 0.5rem;
+	}
+	.left-nav {
+		display: flex;
+		flex-direction: row;
+        vertical-align: middle;
+	}
 </style>

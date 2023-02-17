b6903a42efceddce34b777e3f4d8a521398f8261
Johannes Bähr <b_and_j_gamers@gmx.de>
2023-02-17 18:51:04 +0100

    changed some styles on the index page

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,14 +1,38 @@
 <nav>
-    <p>1</p>
-    <p>2</p>
-    <p>3</p>
+	<div class="right-nav">
+        logo
+    </div>
+	<div class="left-nav">
+		<p>Features</p>
+		<p>Fuer Unternehme</p>
+		<button class="login-button">Login</button>
+	</div>
 </nav>
 
 <style>
-    nav {
+	nav {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between; 
+	}
+    .left-nav > p {
+        margin-right: 1.25rem;
+        text-decoration: underline;
+    }
+    .left-nav > p:hover {
+        text-decoration: none;
+        cursor: pointer;
+    }
+    .login-button {
+        border: none;
+        background-color: white;
+        cursor: pointer;
+        font-size: 1rem;
+        margin-right: 0.5rem;
+    }
+    .left-nav {
         display: flex;
         flex-direction: row;
-        justify-content: flex-end;
-        
     }
+
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,5 @@
 <script>
-    import Navbar from '../components/Navbar.svelte'
+	import Navbar from '../components/Navbar.svelte';
 </script>
 
 <Navbar />
-
-<h1>Hello World</h1>
-
-<style>
-    h1 {
-        text-align: center;
-    }
-</style>

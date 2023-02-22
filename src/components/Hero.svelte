85b329214db89ff96428baa90e70a37f5b452004
Johannes Bähr <b_and_j_gamers@gmx.de>
2023-02-22 23:27:08 +0100

    added hero animation

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,16 +1,30 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	let card: HTMLElement;
+	let card: HTMLElement | null;
+	let heroContent: any;
 
 	onMount(() => {
 		document.addEventListener('mousemove', (e: MouseEvent) => {
-            rotateElement(e, card);
+			rotateElement(e, card);
 		});
-		
+
+		let observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					console.log(entry);
+					entry.target.classList.add('hero-content-wrapper');
+					console.log(entry.target.classList);
+				}
+			});
+		});
+
+		// TODO: welcher type hat diese element, es it kein HTMLElement
+
+		observer.observe(heroContent);
 	});
 
-	function rotateElement(event: MouseEvent, element: HTMLElement) {
+	function rotateElement(event: MouseEvent, element: HTMLElement | null) {
 		// get mouse position
 		const x = event.clientX;
 		const y = event.clientY;
@@ -28,13 +42,13 @@
 		// console.log(offsetX, offsetY);
 
 		// set rotation
-		element.style.setProperty('--rotateX', offsetX + 'deg');
-		element.style.setProperty('--rotateY', -1 * offsetY + 'deg');
+		element?.style.setProperty('--rotateX', offsetX + 'deg');
+		element?.style.setProperty('--rotateY', -1 * offsetY + 'deg');
 	}
 </script>
 
 <section id="hero" class="hero">
-	<div class="hero-content-wrapper">
+	<div bind:this={heroContent} class=" hero-content-wrapper-before-intersecting">
 		<h1 class="hero-title">GSGBANK</h1>
 		<p class="hero-subtitle">Die Bank ihres Vertrauens.</p>
 	</div>
@@ -43,6 +57,8 @@
 		<img class="card" src="/schollkassenkarte_new.png" alt="Schollkassen Karte" />
 	</div>
 </section>
+
+<p class="hero-content-wrapper" style="display: none;" />
 
 <style>
 	.hero {
@@ -64,30 +80,42 @@
 	.card-wrapper {
 		display: none;
 	}
+	.hero-content-wrapper-before-intersecting {
+		opacity: 0;
+		transform: translateY(1rem);
+		transition: opacity 1s ease-in-out, transform 1s ease-in-out;
+		margin: 0 0 0 10dvw;
+
+	}
+	.hero-content-wrapper {
+		opacity: 1;
+		transform: translateY(0);
+	}
 	@media (min-width: 800px) {
-		.hero {
-			display: flex;
-			flex-direction: row;
-			justify-content: space-around;
-		}
 		.hero-title {
 			font-size: 6rem;
 		}
 		.hero-subtitle {
 			font-size: 2.75rem;
 		}
+	}
+	@media (min-width: 1250px) {
+		.hero {
+			display: flex;
+			flex-direction: row;
+			justify-content: space-around;
+		}
+
 		.card-wrapper {
 			display: flex;
+			margin: auto 0 auto 0;
 		}
-        .hero-content-wrapper {
-            margin: 0 0 0 10dvw;
-        }
 		.card {
+			top: 70rem;
 			width: auto;
 			height: auto;
 			max-height: 20dvw;
-            transform: perspective(5000px) rotateY(var(--rotateX)) rotateX(var(--rotateY));
-
+			transform: perspective(5000px) rotateY(var(--rotateX)) rotateX(var(--rotateY));
 		}
 	}
 </style>

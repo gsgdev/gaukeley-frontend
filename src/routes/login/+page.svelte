127c9aa18c49ebc946b4f252b8f26ac6e0f4d0ec
Johannes Bähr <b_and_j_gamers@gmx.de>
2023-02-25 14:12:10 +0100

    styled the login page on desktop

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -16,13 +16,46 @@
 <div class="login-wrapper">
 	<form on:submit|preventDefault={handleSubmit}>
 		<label for="username">Benutzername</label>
-		<div class="input-wrapper">
-			<input placeholder="Benutzername" type="text" id="username" bind:value={username} />
+		<div class="underline">
+			<div class="input-wrapper">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="icon"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+					/>
+				</svg>
+
+				<input placeholder="Benutzername" type="text" id="username" bind:value={username} />
+			</div>
 		</div>
-		
 		<label for="password">Passwort</label>
-		<div class='input-wrapper'>
-			<input placeholder="Passwort" type="password" id="password" bind:value={password} />
+
+		<div class="underline">
+			<div class="input-wrapper">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="icon"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
+					/>
+				</svg>
+				<input placeholder="Passwort" type="password" id="password" bind:value={password} />
+			</div>
 		</div>
 		<button class="submit-button" type="submit">Submit</button>
 	</form>
@@ -49,24 +82,34 @@
 	input {
 		width: 100%;
 		padding: 1rem;
-		border-radius: 10px;
 		border: none;
 		font-size: 1.5rem;
 	}
-	.input-wrapper::after {
+	.icon {
+		background-color: white;
+		width: 3rem;
+		padding: 0 0 0 0.5rem;
+	}
+	.input-wrapper {
+		display: flex;
+		flex-direction: row;
+		border-radius: 20px;
+	}
+	.underline::after {
 		content: '';
 		display: block;
 		height: 2px;
 		background: black;
-		width: 0;
+		width: 0%;
 		bottom: 0;
-		transition: width 0.3 ease-in-out;
-
+		transition: width 0.25s ease-in-out;
 	}
 	input:focus {
 		border: none;
+		outline: none;
 	}
-	.input-wrapper:focus::after {
+	.underline:focus-within::after {
+		content: '';
 		width: 100%;
 	}
 	label {
